8eac3bb8fb0831eb2db1176f4d7f5e28ce7c86aa
pritom <chakmapritom1@gmail.com>
2024-12-21 03:58:08 +0600

    show the all data on details pages

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -3,11 +3,11 @@ import { format } from "date-fns";
 import { Link } from "react-router-dom";
 
 const JobCard = ({ job }) => {
-  const { title, category, deadLine, description, min_price, max_price } =
+  const { title, category, deadLine, description, min_price, max_price ,_id} =
     job || {};
   return (
     <Link
-      to={`/job/1`}
+      to={`/job/${_id}`}
       className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md hover:scale-[1.05] transition-all"
     >
       <div className="flex items-center justify-between">
